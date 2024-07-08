d87f949893f1bb557d991d5b07d65ae0979fed03
OMOTEC <151024042+OMOTEC-ELECTRONICS@users.noreply.github.com>
2024-07-08 15:26:27 +0530

    Update Activity_11_Consol_Controlled_Automation.py

--- CHAPTER_05 - Console & Vision Control/Activity-11_Consol_Controlled_Automation_System/Activity_11_Consol_Controlled_Automation.py
+++ CHAPTER_05 - Console & Vision Control/Activity-11_Consol_Controlled_Automation_System/Activity_11_Consol_Controlled_Automation.py
@@ -4,7 +4,7 @@
 import serial
 import time
 
-arduino = serial.Serial('com3', 9600)
+arduino_ide = serial.Serial('com6', 9600) #Change the port name as per arduino port 
 time.sleep(2)  # Wait for the communication to get established
 
 print("Enter 1 to turn LED ON or 0 to turn LED OFF")
@@ -12,12 +12,12 @@ print("Enter 1 to turn LED ON or 0 to turn LED OFF")
 while True:
     var = input("Enter your choice: ")  # Wait for user input
     print(var)
-
+    
     if var == '1':
-        arduino.write(b'1')  # Send '1' to turn LED ON
+        arduino_ide.write(b'1')  # Send '1' to turn LED ON
         print("LED turned Off")
     elif var == '0':
-        arduino.write(b'0')  # Send '0' to turn LED OFF
+        arduino_ide.write(b'0')  # Send '0' to turn LED OFF
         print("LED turned On")
     else:
         print("Invalid input")
