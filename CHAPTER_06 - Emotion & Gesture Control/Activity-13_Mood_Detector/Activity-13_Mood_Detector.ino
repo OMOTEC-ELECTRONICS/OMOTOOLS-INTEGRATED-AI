3996c51f1d61eb644531e31b371207ea7fffd197
OMOTEC <151024042+OMOTEC-ELECTRONICS@users.noreply.github.com>
2024-07-13 12:11:11 +0530

    Update Activity-13_Mood_Detector.ino

--- CHAPTER_06 - Emotion & Gesture Control/Activity-13_Mood_Detector/Activity-13_Mood_Detector.ino
+++ CHAPTER_06 - Emotion & Gesture Control/Activity-13_Mood_Detector/Activity-13_Mood_Detector.ino
@@ -1,21 +1,26 @@
-int greenLedPin = D2;
+
+int LedPin = D2; //Initialize the LED pin
 
 
 void setup() {
-  pinMode(greenLedPin, OUTPUT);
-  
-  Serial.begin(9600);
+  pinMode(LedPin, OUTPUT); //Set the mode of the LedPin as Output pin
+ 
+  Serial.begin(9600); //Call serial monitor
 }
 
+
 void loop() {
-  if (Serial.available() > 0) {
-    char command = Serial.read();
-    if (command == '1') {
-      digitalWrite(greenLedPin, HIGH);  // Turn green LED on
-          
-    } else if (command == '0') {
-      digitalWrite(greenLedPin, LOW);   // Turn green LED off
-       
+  if (Serial.available() > 0) 
+{
+    char command = Serial.read(); //Create new variable to store the serial monitor reading
+    if (command == '1')   
+{
+      digitalWrite(LedPin, HIGH);  // Turn LED on
+         
+    } 
+else if (command == '0') 
+{
+      digitalWrite(LedPin, LOW);   // Turn LED off
     }
   }
 }
