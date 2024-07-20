8fe2b83456e7bc032ae0fff062ca24cd290f4b94
OMOTEC <151024042+OMOTEC-ELECTRONICS@users.noreply.github.com>
2024-07-20 10:13:54 +0530

    Update Activity-14_Gesture_Recognizer.ino

--- CHAPTER_06 - Emotion & Gesture Control/Activity-14_Gesture_Recognizer/Activity-14_Gesture_Recognizer.ino
+++ CHAPTER_06 - Emotion & Gesture Control/Activity-14_Gesture_Recognizer/Activity-14_Gesture_Recognizer.ino
@@ -1,25 +1,32 @@
-Int LED = D2;
-void setup() {
-  Serial.begin(9600); // Initialize serial communication
-pinMode (LED, OUTPUT);
+int LED = D2;
+void setup() 
+{
+Serial.begin(9600); // Initialize serial communication
+pinMode (LED, OUTPUT); //LED is set as output component
 }
 
-void loop() {
-
-  if (Serial.available() > 0) {
-    char command = Serial.read(); // Read the command from serial
-    if (command == 'H') {
-      // Perform action for "Hi" gesture
-      Serial.println("Received command: Hi");
-   digitalWrite (LED, HIGH);
-    } else if (command == 'N') {
-      // Perform action for "Namaste" gesture
-      Serial.println("Received command: Namaste");
-  digitalWrite (LED, HIGH);
-    } else {
-      // Unknown command
-      Serial.println("Unknown command received");
-  digitalWrite (LED, LOW);
-    }
-  }
+void loop() 
+{
+if (Serial.available() > 0) 
+{
+char command = Serial.read(); // Read the command from serial
+if (command == 'H') 
+{
+// Perform action for "Hi" gesture
+Serial.println("Received command: Hi");
+digitalWrite (LED, HIGH);
+} 
+else if (command == 'N') 
+{
+// Perform action for "Namaste" gesture
+Serial.println("Received command: Namaste");
+digitalWrite (LED, HIGH);
+} 
+else 
+{
+// Unknown command
+Serial.println("Unknown command received");
+digitalWrite (LED, LOW);
+}
+}
 }
